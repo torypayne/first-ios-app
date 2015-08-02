ece83f76ac5d291c572bfde1569ec8529d7c452b
Victoria Payne <vhpayne@gmail.com>
2015-08-02 11:38:06 -0700

    First commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Victoria Payne on 8/1/15.
+//  Copyright (c) 2015 Tory Payne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Record user voice
+        //TODO: Show text "recording in progress"
+        println("in recordAudio")
+    }
+
+}
+
